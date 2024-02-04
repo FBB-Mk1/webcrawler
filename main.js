23b7218a0b2c14632a27d1c6dcd4dc06782c0be6
fabin <fabiobatista@gmail.com>
2024-02-04 10:50:17 -0300

    refactor: error naming

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const { getURLsFromHTML, crawlPage } = require("./crawl")
 async function main() {
 
     if (process.argv.length !== 3) {
-        console.log("Invalid Input, need a valid URL")
+        console.log("Invalid Input, too many/few arguments")
         process.exit()
     }
     let url
     try {
         url = new URL(process.argv[2])
     } catch (err) {
-        console.log(err)
+        console.log(`${err}: Invalid URL`)
     }
     let result = await crawlPage(url, url)
 
